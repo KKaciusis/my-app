2b7ad4f59a2a5d72ebbeaed9fb44a9dc031baa11
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-19 17:27:28 +0300

    Working color change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,33 @@
 import React from 'react';
-import ChangeColorButton from './ChangeColorButton';
+
 
 class App extends React.Component {
     constructor(){
         super();
         this.state ={
             bg: 'palegreen',
-            in: 'Choose Color'
+            bgIn: ''
         };
     }
-    changeColorP = () => {
-        this.setState({
-            bg: 'palegreen',
-        });
-    }
 
-    changeColorO = () => {
+    inChangeColor = (e) => {
         this.setState({
-            bg: 'orangered',
-        });
-    }
-
-    changeColorG = () => {
-        this.setState({
-            bg: 'greenyellow',
-        });
-    }
-
-    changeColor = (color) => {
-        this.setState({
-            bg: color,
-        });
-    }
-
-    inChange = (e) => {
-        this.setState({
-            in: e.target.value,
+            bgIn: e.target.value,
         });
     }
 
     doColor = () => {
-        this.setState(state => ({bg: state.in}));
+        this.setState(state => ({bg: state.bgIn}));
     }
 
 
     render() {
         return (
             <div className="circle" style={{backgroundColor: this.state.bg}}>
-                <input type="text" value={this.state.in} onChange={this.inChange}/>
+                <div>
+                <input type="text" value={this.state.bgIn} onChange={this.inChangeColor}/>
                 <button className="input-button" onClick={this.doColor}>Change Color</button>
+            </div>
             </div>
         );
     }
