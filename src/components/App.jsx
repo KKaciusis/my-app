5b6085d0c61465b2b46d8a2ffe187e31427d07f6
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-19 17:37:32 +0300

    working size change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,25 @@ class App extends React.Component {
     constructor(){
         super();
         this.state ={
-            bg: 'palegreen',
-            bgIn: ''
+            bg: 'white',
+            bgIn: 'color input',
+            size: 100,
+            sizeIn: 'size input'
         };
+    }
+    inChangeSize = (e) => {
+        this.setState({
+            sizeIn: e.target.value,
+        });
     }
 
     inChangeColor = (e) => {
         this.setState({
             bgIn: e.target.value,
         });
+    }
+    doSize = () => {
+        this.setState(state => ({size: state.sizeIn}));
     }
 
     doColor = () => {
@@ -23,9 +33,11 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="circle" style={{backgroundColor: this.state.bg}}>
+            <div className="circle" style={{backgroundColor: this.state.bg, width: this.state.size+'px', height: this.state.size+'px'}}>
                 <div>
                 <input type="text" value={this.state.bgIn} onChange={this.inChangeColor}/>
+                <input type="text" value={this.state.sizeIn} onChange={this.inChangeSize}/>
+                <button className="input-button" onClick={this.doSize}>Change Size</button>
                 <button className="input-button" onClick={this.doColor}>Change Color</button>
             </div>
             </div>
