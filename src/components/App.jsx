fd724b917d5ce8a8755707d1083105ec86ba8633
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-21 19:48:53 +0300

    deletion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SmallShape from './SmallShape';
+import getId from '../shared/id';
 
 class App extends React.Component {
     constructor(){
@@ -11,9 +12,23 @@ class App extends React.Component {
         };
     }
     addShape = (s) =>{
-        const shape = {color: this.state.circleIn, shape: s};
+        const shape = {id: getId(), color: this.state.circleIn, shape: s};
         const shapes = this.state.shapes.slice();
         shapes.push(shape);
+        this.setState({
+            shapes: shapes
+        })
+        localStorage.setItem('allShapes', JSON.stringify(shapes))
+    }
+    deleteShape = (id) => {
+        console.log(id)
+        const shapes = this.state.shapes.slice();
+        for(let i = 0; i < shapes.length; i++){
+            if(shapes[i].id === id){
+                shapes.splice(i, 1);
+                break;
+            }
+        }
         this.setState({
             shapes: shapes
         })
@@ -39,7 +54,7 @@ class App extends React.Component {
     render() {
         return (
             <>
-            {this.state.shapes.map((b, i)=> <SmallShape key={i} color={b.color} shape={b.shape}/>)}
+            {this.state.shapes.map(b => <SmallShape key={b.id} delete={this.deleteShape} id={b.id} color={b.color} shape={b.shape}/>)}
             <div>
                     <input type="text" value={this.state.circleIn} onChange={this.circleInputHandler}/>
                     <button className="input-button" onClick={()=>this.addShape('circle')}>Add Circle</button>
--- src/components/SmallShape.jsx
+++ src/components/SmallShape.jsx
@@ -1,4 +1,26 @@
-function SmallShape(props) {
-    return (<div className="small-circle" style={{backgroundColor: props.color, borderRadius: props.shape === "circle" ? '50%' : '5px'}}>{props.color}</div>);
-}
-export default SmallShape;
+import React from 'react';
+
+class SmallShape extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            editIn: ''
+
+        };
+    }
+render(){
+    return (<div
+    className="small-circle"
+        style={{
+            backgroundColor: this.props.color,
+            borderRadius: this.props.shape === "circle" ? '50%' : '5px'
+        }}>
+            <span>{this.props.color}</span>
+            <button className="input-button-small" onClick={()=>this.props.delete(this.props.id)}>Delete</button>
+            <div className="center">
+            <input type="text"></input>
+            <button className="input-button-small" onClick={()=>this.props.delete(this.props.id)}>edit color</button>
+            </div>
+            </div>);
+}};
+export default SmallShape
