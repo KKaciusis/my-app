7a48f10e982ebe4ef541b1056dd62fae0943f58d
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-22 18:58:21 +0300

    finished location change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,67 +6,95 @@ function App() {
     
     const [shapes, setShape] = useState([]);
     const [colorIn, setColorIn] = useState('');
-    useEffect(()=>{
 
-    });
+    const  addShape = (s) => {
 
-  const  addShape = (s) =>{
-        const shape = {id: getId(), color: colorIn, shape: s};
+        const shape = {id: getId(), color: colorIn, shape: s, location1: true};
         const shapess = shapes.slice();
+        
         shapess.push(shape);
-        setShape(shapess)
+        setShape(shapess);
         localStorage.setItem('allShapes', JSON.stringify(shapess));
     }
-  const  deleteShape = (id) => {
-        console.log(id)
+
+    const  deleteShape = (id) => {
+        
         const shapess = shapes.slice();
+
         for(let i = 0; i < shapess.length; i++){
             if(shapes[i].id === id){
+
                 shapess.splice(i, 1);
                 break;
             }
         }
-        setShape(shapess)
+
+        setShape(shapess);
         localStorage.setItem('allShapes', JSON.stringify(shapess));
-    }
+    };
+
    const editShape = (id, color) => {
-        console.log(id)
+        
         const shapess = shapes.slice();
+
         for(let i = 0; i < shapess.length; i++){
             if(shapess[i].id === id){
                 shapess[i].color = color;
                 break;
             }
         }
+
         setShape(shapess)
         localStorage.setItem('allShapes', JSON.stringify(shapess));
-    }
+    };
+
+    const changeLocation= (id) => {
+        const shapess = shapes.slice();
+
+        for(let i = 0; i < shapess.length; i++){
+            if(shapess[i].id === id){
+                shapess[i].location1 = !shapess[i].location1;
+                break;
+            }
+        }
+        setShape(shapess);
+        localStorage.setItem('allShapes', JSON.stringify(shapess));
+    };
 
    const colorInputHandler = (e) => {
         setColorIn(e.target.value);
-    }
+    };
 
     useEffect(() => {
+
         const shapess = JSON.parse(localStorage.getItem('allShapes'));
         if (null === shapess){
             return;
         }
+
         setShape(shapess)
-    }, [])
+    }, []);
 
-        return (
-            <>
-            {shapes.map(b => <SmallShape key={b.id} delete={deleteShape} edit={editShape} id={b.id} color={b.color} shape={b.shape}/>)}
-            <div>
-                    <input type="text" value={colorIn} onChange={colorInputHandler}/>
-                    <button className="input-button" onClick={()=> addShape('circle')}>Add Circle</button>
-                    <button className="input-button" onClick={()=> addShape('sq')}>Add Sq</button>
+    return (
+        <>
+        <div className="field">
+            <div className="farm">
+            <h3>Location 1</h3>
+            {shapes.map(b => <SmallShape location1={b.location1} locationNumber={1} key={b.id} delete={deleteShape} change={changeLocation} edit={editShape} id={b.id} color={b.color} shape={b.shape}/>)}
             </div>
-            </>
+            <div className="farm">
+            <h3>Location 2</h3>
+            {shapes.map(b => <SmallShape location1={b.location1} locationNumber={2} key={b.id} delete={deleteShape} change={changeLocation} edit={editShape} id={b.id} color={b.color} shape={b.shape}/>)}
+            </div>
+            </div>
+        <div>
+                <input type="text" value={colorIn} onChange={colorInputHandler}/>
+                <button className="input-button" onClick={()=> addShape('circle')}>Add Circle</button>
+                <button className="input-button" onClick={()=> addShape('sq')}>Add Sq</button>
+        </div>
+        </>
 
-        );
+    );
 }
-
-
 
 export default App;
--- src/components/SmallShape.jsx
+++ src/components/SmallShape.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from 'react';
 
-function SmallShape (props){
+function SmallShape (props) {
 
     const [editIn, setEditIn] = useState('');
 
     const editInputHandler = (e) => {
+
         setEditIn(e.target.value);
     }
-    return (<div
-    className="small-circle"
-        style={{
-            backgroundColor: props.color,
-            borderRadius: props.shape === "circle" ? '50%' : '5px'
-        }}>
-            <span>{props.color}</span>
-            <button className="input-button-small" onClick={()=>props.delete(props.id)}>Delete</button>
-            <div className="center">
-            <input type="text" value={editIn} onChange={editInputHandler}></input>
-            <button className="input-button-small" onClick={()=> props.edit(props.id, editIn)}>edit color</button>
-            </div>
-            </div>);
+
+
+    if((props.locationNumber === 1 && props.location1) || (props.locationNumber === 2 && !props.location1)) {
+
+        return (
+        <div
+        className="small-circle"
+            style={{
+                backgroundColor: props.color,
+                borderRadius: props.shape === "circle" ? '50%' : '5px'
+            }}>
+
+                <span>{props.color}</span>
+                <button className="input-button-small" onClick={()=>props.delete(props.id)}>Delete</button>
+                <button className="input-button-small" onClick={() => props.change(props.id)}>Change location</button>
+                <div className="center">
+                <input type="text" value={editIn} onChange={editInputHandler}></input>
+                <button className="input-button-small" onClick={()=> props.edit(props.id, editIn)}>Edit color</button>
+                </div>
+                </div>);
+    } else {
+        return (<></>)
+    }
 };
+
 export default SmallShape;
